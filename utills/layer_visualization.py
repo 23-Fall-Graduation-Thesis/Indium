5bfbdd9a530daa19e2cf95f411da16210ada2e34
vaughn <jwst0208@gmail.com>
2023-11-12 17:01:29 +0900

    modify: visTensor

--- utills/layer_visualization.py
+++ utills/layer_visualization.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 from torchvision import utils
 import numpy as np
 
-def visTensor(tensor, ch=0, allkernels=False, nrow=8, padding=1): 
+"""def visTensor(tensor, ch=0, allkernels=False, nrow=8, padding=1): 
     n,c,w,h = tensor.shape
     if allkernels: tensor = tensor.view(n*c, -1, w, h)
     elif c != 3: tensor = tensor[:,ch,:,:].unsqueeze(dim=1)
@@ -12,7 +12,20 @@ def visTensor(tensor, ch=0, allkernels=False, nrow=8, padding=1):
     grid = utils.make_grid(tensor, nrow=nrow, normalize=True, padding=padding)
     plt.figure( figsize=(nrow,rows) )
     plt.imshow(grid.numpy().transpose((1, 2, 0)))
-    return
+    return"""
+
+def visTensor(tensor, ncols=8 , showAll=False):
+    # only use FIRST CHANNEL #TODO 
+    n, c, w, h = tensor.shape
+    nrows = n // ncols + (1 if n % ncols else 0)
+    
+    fig, axes = plt.subplots(nrows=nrows, ncols=ncols, figsize=(ncols, nrows))
+    for i in range(n):
+        ax = axes[i // ncols, i % ncols]
+        kernel = tensor[i, 0, :, :] if n > 1 else tensor[i, :, :]
+        ax.imshow(kernel, cmap='viridis')
+        ax.axis('off')
+    plt.show()
 
 def filters_visualize(model, layer_name, nrows=1, ncols=6):
     flag = False
@@ -25,10 +38,9 @@ def filters_visualize(model, layer_name, nrows=1, ncols=6):
         print(f'Undefined Layer.')
         return
     
+    print(weights.shape)
     visTensor(weights)
-    plt.axis('off')
-    plt.ioff()
-    plt.show()
+
     
 def activation_visualize(model):
     ""
