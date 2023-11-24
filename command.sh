fa14b642068680e521d57905b0143d410499e00d
somi <handong227@gmail.com>
2023-11-24 20:09:29 +0900

    modify: command.sh

--- command.sh
+++ command.sh
@@ -1,2 +1,5 @@
-# cifar10 - Conv4
-python3 -u main.py --dataset cifar10 --model resnet18 --mode cus --epoch 100 -lr 0.001
+# Pretraining
+python3 -u main.py --dataset cifar10 --model Conv4 --pretrain true --epoch 100 --lr 0.001
+
+# Custom Fine-tuning
+python3 -u main.py --dataset cifar10 --model resnet18 --mode cus --epoch 100 --lr 0.001
