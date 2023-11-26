69f61b1607b0b670a002d20aa6d5665068be07fe
webb-c <jwst0208@gmail.com>
2023-11-27 05:23:44 +0900

    add: weight distribution & add: requirement Pillow version

--- utils/get_data.py
+++ utils/get_data.py
@@ -2,31 +2,30 @@ import torch
 import torch.nn as nn
 import pandas as pd
 
-def get_all_feature_maps(model, image):
-    feature_maps = []
-    hooks = []
+def get_weights(model, layer_name):
+    flag = False
+    for name, layer in model.named_modules():
+        if name == layer_name:
+            flag = True
+            weights = layer.weight.data.clone()
+            break
+    if not flag :
+        print(f'Undefined Layer.')
+        return -1
+    return weights
 
-    def hook_fn(module, input, output):
-        if isinstance(module, torch.nn.Conv2d):
-            feature_maps.append(output)
 
-    for layer in model.children():
-        if isinstance(layer, torch.nn.Conv2d):
-            hook = layer.register_forward_hook(hook_fn)
-            hooks.append(hook)
+def get_feature_map(model, input, layer_name, idx):
+    def get_activation(name):
+        def hook(model, input, output):
+            activation[name] = output.detach()
+        return hook
 
-    with torch.no_grad():
-        model(image)
-
-    for hook in hooks:
-        hook.remove()
-
-    return feature_maps
-
-def get_feature_map(model, image):
-    with torch.no_grad(): 
-        feature_map = model(image)
-    return feature_map
+    model.eval()
+    model.features[idx].register_forward_hook(get_activation(layer_name))
+    output = model(input)
+    act = activation[layer_name].squeeze()
+    return act
 
 
 def get_numerical_weight(model):
@@ -36,7 +35,8 @@ def get_numerical_weight(model):
     layer_index = []
 
     conv_layer_num = 0
-    for layer in model.children():
+
+    for name, layer in model.named_modules():
         if isinstance(layer, nn.Conv2d):
             conv_layer_num += 1
             weights = layer.weight.data.cpu().numpy().flatten()
@@ -48,44 +48,3 @@ def get_numerical_weight(model):
     weight_df = pd.DataFrame({'Layer': layer_index, 'Weights': weights_data})
 
     return means, variances, weight_df
-
-def get_activation(name):
-    def hook(model, input, output):
-        activation[name] = output.detach()
-    return hook
-
-#
-#
-#
-
-def hook_fn(module, input, output):
-    module.output = output
-
-def addHook(model, hook):
-    for layer in model.children():
-        if len(list(layer.children())) > 0: # if module has children -> sequential
-            addHook(layer, hook)
-
-        else:
-            layer.register_forward_hook(hook)
-
-def collectOutput(model, output_list):
-    for layer in model.children():
-        if len(list(layer.children())) > 0: # if module has children -> sequential
-            collectOutput(layer, output_list)
-        
-        elif type(layer) == nn.ParameterList:
-            return
-
-        else:
-            output = layer.output
-            output_list.append(output)
-
-def getName(model, name_list):
-    for layer in model.children():
-        if len(list(layer.children())) > 0: # if module has children -> sequential
-            getName(layer, name_list)
-
-        else:
-            layer_name = str(layer)[:str(layer).index("(")] + str(len(list(filter(lambda x:str(layer)[:str(layer).index("(")] in x, name_list))))
-            name_list.append(layer_name)
--- utils/visualize_tool.py
+++ utils/visualize_tool.py
@@ -3,68 +3,75 @@ import seaborn as sns
 from torchvision import utils
 import numpy as np
 import torch
-from utils.get_data import get_all_feature_maps, get_numerical_weight
+from utils.get_data import *
+from utils.util_functions import *
 
-def visTensor(tensor, layer_name, ncols=32 , showAll=False):
-    # only use FIRST CHANNEL #TODO 
-    n, c, w, h = tensor.shape
+def visualize_tensor(tensor, isnomalized=True):
+    numpy = tensor.numpy().transpose(1, 2, 0)
+    if isnomalized :
+        numpy = (numpy - numpy.min()) / (numpy.max() - numpy.min())
+    plt.imshow(numpy)
+    plt.show()
+
+
+def plot_filters(tensor, layer_name, ncols=32 , nchannel=5):
+    n, _, _, _ = tensor.shape
     nrows = n // ncols + (1 if n % ncols else 0)
-    
+
+    for c in range(nchannel):
+        fig, axes = plt.subplots(nrows=nrows, ncols=ncols, figsize=(ncols, nrows))
+        for i in range(n):
+            ax = axes[i // ncols, i % ncols]
+            kernel = tensor[i, c, :, :] if n > 1 else tensor[i, :, :]
+            ax.imshow(kernel, cmap='viridis')
+            ax.axis('off')
+        alias = get_alias(layer_name, model_info)
+        plt.suptitle('Layer: '+alias+" (# channel: "+str(c)+")")
+        plt.show()
+
+
+def visualize_filters(model, layer_name, ncols=32, nchannel=5, showAll=False):
+    weights = get_weights(model, layer_name)
+    print(weights.shape)
+    n, c, w, h = weights.shape
+    if not showAll:
+        c = min(c, nchannel)
+    plot_filters(weights, layer_name, ncols, c)
+
+
+
+def visualize_feature_map(model, input, layer_name, idx, ncols=32):
+    act = get_feature_map(model, input, layer_name, idx)
+    print(act.shape)
+    nrows = max(act.size(0) // ncols, 1) 
     fig, axes = plt.subplots(nrows=nrows, ncols=ncols, figsize=(ncols, nrows))
-    for i in range(n):
+    for i in range(act.size(0)):
         ax = axes[i // ncols, i % ncols]
-        kernel = tensor[i, 0, :, :] if n > 1 else tensor[i, :, :]
+        kernel = act[i]
         ax.imshow(kernel, cmap='viridis')
         ax.axis('off')
-    plt.title('Layer', layer_name)
+    plt.suptitle('Layer: '+layer_name)
     plt.show()
 
 
-def filters_visualize(model, layer_name, ncols=32, showAll=False):
-    flag = False
-    for name, layer in model.named_modules():
-        if name == layer_name:
-            flag = True
-            weights = layer.weight.data.clone()
-            break
-    if not flag :
-        print(f'Undefined Layer.')
-        return
-    
-    print(weights.shape)
-    visTensor(weights, ncols, showAll)
-
-    
-def feature_map_visualize(model, image, cols=32):
-    feature_maps = get_all_feature_maps(model, image)
-    num_layers = len(feature_maps)    
-    rows = num_layers // cols + (1 if num_layers % cols else 0)
-    plt.figure(figsize=(20, rows * 3))
-    for i, feature_map in enumerate(feature_maps):
-        plt.subplot(rows, cols, i+1)
-        plt.imshow(feature_map[0, 0].cpu().detach(), cmap='viridis')
-        plt.title(f'Layer {i+1}')
-        plt.axis('off')
-
-    plt.tight_layout()
-    plt.show()
-
-
-def weight_distribution_visualize(model):
+def visualize_weight_distribution(model, violin_sample=1000):
+    sns.set()
     means, variances, weight_df = get_numerical_weight(model)
     
-    plt.figure(figsize=(10, 5))
+    plt.figure(figsize=(7, 4))
     plt.plot(means, label='Mean')
     plt.plot(variances, label='Variance')
-    plt.xlabel('Kernel Index')
-    plt.ylabel('Value')
-    plt.title('Kernel Weights Statistics (Mean and Variance)')
+    plt.xlabel('conv layer idx')
+    plt.ylabel('value')
+    plt.title('Weights Statistics')
     plt.legend()
     plt.show()
 
-    plt.figure(figsize=(12, 6))
-    sns.violinplot(x='Layer', y='Weights', data=weight_df)
-    plt.xlabel('Convolution Layer Number')
-    plt.ylabel('Weights')
-    plt.title('Weight Distribution in Convolutional Layers')
+
+    plt.figure(figsize=(7, 4))
+    sampled_df = weight_df.groupby('Layer').apply(lambda x: x.sample(violin_sample)).reset_index(drop=True)
+    plt.xlabel('conv #')
+    plt.ylabel('weights')
+    plt.xticks([1, 2, 3, 4, 5], ['1', '2', '3', '4', '5'])
+    sns.violinplot(x='Layer', y='Weights', data=sampled_df)
     plt.show()
