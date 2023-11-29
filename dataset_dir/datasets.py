e47afb7ff12ddcca66e4ef5cb45f86a3738093e7
youuijin <80818313+youuijin@users.noreply.github.com>
2023-11-29 14:27:39 +0900

    fix: dataloader shuffle option add

--- dataset_dir/datasets.py
+++ dataset_dir/datasets.py
@@ -4,19 +4,19 @@ import torchvision.transforms as transforms
 from torch.utils.data import DataLoader, random_split
 from dataset_dir.cub200 import Cub2011
 
-def datasetload(dataset_name):
+def datasetload(dataset_name, shuffle=True):
     if dataset_name == 'cifar10':
-        return Cifar10()
+        return Cifar10(shuffle)
     elif dataset_name == 'cifar100':
-        return Cifar100()
+        return Cifar100(shuffle)
     elif dataset_name == 'svhn':
-        return SVHN()
+        return SVHN(shuffle)
     elif dataset_name == 'cub':
-        return CUB200()
+        return CUB200(shuffle)
 
 
 
-def Cifar10():
+def Cifar10(shuffle):
     n_class = 10
 
     transform = transforms.Compose([
@@ -34,17 +34,17 @@ def Cifar10():
     valid_size = dataset_size - train_size
     trainset, validset = random_split(train_dataset, [train_size, valid_size])
     
-    trainloader = DataLoader(trainset, batch_size, shuffle=True, drop_last=True, num_workers=2)
-    validloader = DataLoader(validset, batch_size, shuffle=True, drop_last=True, num_workers=2)
+    trainloader = DataLoader(trainset, batch_size, shuffle=shuffle, drop_last=True, num_workers=2)
+    validloader = DataLoader(validset, batch_size, shuffle=shuffle, drop_last=True, num_workers=2)
     
     testset = torchvision.datasets.CIFAR10(root='./data/cifar10', train=False, download=True, transform=transform)
-    testloader = DataLoader(testset, batch_size, shuffle=False, drop_last=True, num_workers=2)
+    testloader = DataLoader(testset, batch_size, shuffle=shuffle, drop_last=True, num_workers=2)
     
     # classes = {'plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck'}
     
     return trainloader, validloader, testloader, n_class
 
-def Cifar100():
+def Cifar100(shuffle):
     n_class = 100
 
     transform = transforms.Compose([
@@ -62,16 +62,16 @@ def Cifar100():
     valid_size = dataset_size - train_size
     trainset, validset = random_split(train_dataset, [train_size, valid_size])
     
-    trainloader = DataLoader(trainset, batch_size, shuffle=True, drop_last=True, num_workers=2)
-    validloader = DataLoader(validset, batch_size, shuffle=True, drop_last=True, num_workers=2)
+    trainloader = DataLoader(trainset, batch_size, shuffle=shuffle, drop_last=True, num_workers=2)
+    validloader = DataLoader(validset, batch_size, shuffle=shuffle, drop_last=True, num_workers=2)
     
     testset = torchvision.datasets.CIFAR100(root='./data/cifar100', train=False, download=True, transform=transform)
-    testloader = DataLoader(testset, batch_size, shuffle=False, drop_last=True, num_workers=2)
+    testloader = DataLoader(testset, batch_size, shuffle=shuffle, drop_last=True, num_workers=2)
     
     return trainloader, validloader, testloader, n_class
     
 
-def SVHN():
+def SVHN(shuffle):
     n_class = 10
     
     transform = transforms.Compose([
@@ -90,13 +90,13 @@ def SVHN():
     valid_svhn = torch.utils.data.Subset(svhn, valid_indices)
     test_svhn = torch.utils.data.Subset(svhn, test_indices)
     
-    trainloader = DataLoader(train_svhn, batch_size, shuffle=True, drop_last=True, num_workers=2)
-    validloader = DataLoader(valid_svhn, batch_size, shuffle=True, drop_last=True, num_workers=2)
-    testloader = DataLoader(test_svhn, batch_size, shuffle=False, drop_last=True, num_workers=2)
+    trainloader = DataLoader(train_svhn, batch_size, shuffle=shuffle, drop_last=True, num_workers=2)
+    validloader = DataLoader(valid_svhn, batch_size, shuffle=shuffle, drop_last=True, num_workers=2)
+    testloader = DataLoader(test_svhn, batch_size, shuffle=shuffle, drop_last=True, num_workers=2)
     
     return trainloader, validloader, testloader, n_class
 
-def CUB200():
+def CUB200(shuffle):
     n_class = 200
 
     transform = transforms.Compose([
@@ -113,10 +113,10 @@ def CUB200():
     valid_size = dataset_size - train_size
     trainset, validset = random_split(train_dataset, [train_size, valid_size])
     
-    trainloader = DataLoader(trainset, batch_size, shuffle=True, drop_last=True, num_workers=2)
-    validloader = DataLoader(validset, batch_size, shuffle=True, drop_last=True, num_workers=2)
+    trainloader = DataLoader(trainset, batch_size, shuffle=shuffle, drop_last=True, num_workers=2)
+    validloader = DataLoader(validset, batch_size, shuffle=shuffle, drop_last=True, num_workers=2)
     
     test_dataset = Cub2011(root='./data/cub-200-2011', transform=transform, download=True, train=False)
-    testloader = DataLoader(test_dataset, batch_size, shuffle=False, drop_last=True, num_workers=2)
+    testloader = DataLoader(test_dataset, batch_size, shuffle=shuffle, drop_last=True, num_workers=2)
     
     return trainloader, validloader, testloader, n_class
